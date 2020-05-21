76ab65004f65b94aa9d819424f6db8b3cf14e85e
rabraghib <49442862+rabraghib@users.noreply.github.com>
2020-05-21 18:52:05 +0000

    add jwt auth

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,10 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ApiResource()
+ * @ApiResource( 
+ *     collectionOperations={"get"={"method"="GET"}},
+ *     itemOperations={"get"}
+ * )
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
 class User implements UserInterface
